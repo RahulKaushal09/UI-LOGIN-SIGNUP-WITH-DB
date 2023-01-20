c18b36780223830208776ee56197df51480ee3b6
RahulKaushal09 <112763067+RahulKaushal09@users.noreply.github.com>
2023-01-20 17:33:26 +0530

    Update login.py

--- login.py
+++ login.py
@@ -8,40 +8,6 @@ import sys
 USERNAME = "agnextmohali"
 PASSWORD = "Agnext"
 
-# class Login(QDialog):
-#     def __init__(self):
-#         super(Login,self).__init__()
-#         loadUi("loginpage.ui",self)
-#         self.Submitbutton_2.clicked.connect(self.Loginfunction)
-
-
-
-#     def Loginfunction(self):
-#         Username_input = self.Username_2.text()
-#         Password_input = self.Password_2.text()
-#         if USERNAME == str(Username_input) and PASSWORD == str(Password_input):
-#             print("right")
-#         else:
-#             print("wrong")
-        
-            
-
-
-
-
-
-
-
-
-# app = QtWidgets.QApplication(sys.argv)
-# uilogin=Login()
-# widget=QtWidgets.QStackedWidget()
-# widget.addWidget(uilogin)
-# widget.show()
-# widget.setFixedHeight(1000)
-# widget.setFixedWidth(1000)
-
-
 
 def loginfunction(self):
     Username_input=ui.Username_2.text()
@@ -89,8 +55,3 @@ ui.submit_signup.clicked.connect(submit_reg)
 sys.exit(app.exec_())
 
 
-# if (Submit):
-#     if USERNAME == Username and PASSWORD == Password:
-#         logged()
-#     else:
-#         failed()
